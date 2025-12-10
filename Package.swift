5ad3abc29e7cc0fc3681830c2e48281741c95853
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-12-10 08:20:41 +0100

    Update dependencies to latest versions

--- Package.swift
+++ Package.swift
@@ -32,8 +32,8 @@ let package = Package(
         .library(name: .iso8601, targets: [.iso8601]),
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-standards/swift-standards", from: "0.1.0"),
-        .package(url: "https://github.com/swift-standards/swift-incits-4-1986", from: "0.1.0"),
+        .package(url: "https://github.com/swift-standards/swift-standards", from: "0.10.0"),
+        .package(url: "https://github.com/swift-standards/swift-incits-4-1986", from: "0.6.2"),
     ],
     targets: [
         .target(
