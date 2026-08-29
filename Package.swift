e239ee08f7aadf1fb451b34e6dba8a5a8488b715
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-29 02:42:06 +0200

    Use canonical standard library extensions repository

--- Package.swift
+++ Package.swift
@@ -42,7 +42,7 @@ let package = Package(
     ],
     dependencies: [
         .package(
-            url: "https://github.com/swift-molecules/swift-standard-library-extensions.git",
+            url: "https://github.com/swift-atoms/swift-standard-library-extensions.git",
             branch: "main"
         ),
         .package(
